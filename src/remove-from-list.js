e3a326886f0c6e6b3c3503137b223461f381ca06
brbrov <epoc@list.ru>
2022-10-18 00:44:04 +0300

    feat: create remove linked list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -25,6 +25,18 @@ const { NotImplementedError } = require('../extensions/index.js');
 function removeKFromList(l, k) {
   // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if (l.value === k) {
+    l = l.next
+  }
+  let link = l
+  while (link.next) {
+    if (link.next.value === k) {
+      link.next = link.next.next
+    } else {
+      link = link.next
+    }
+  }
+  return l
 }
 
 module.exports = {
